8746d794d8bca6cb1aad53c279096644fe6f3fc3
avikas1030 <99540041+avikas1030@users.noreply.github.com>
2024-09-04 21:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,27 +41,17 @@ def transform_text(text):
 
     return " ".join(y)
 
-
-# Load vectorizer and model
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-# Streamlit interface
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-    # 1. Preprocess the input
     transformed_sms = transform_text(input_sms)
-
-    # 2. Vectorize the input
     vector_input = tfidf.transform([transformed_sms])
-
-    # 3. Predict using the model
     result = model.predict(vector_input)[0]
-
-    # 4. Display the result
     if result == 1:
         st.header("Spam")
     else:
